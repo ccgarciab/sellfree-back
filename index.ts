da1b1bb08fed2f22738ee19f6e06b599415e98fa
Cristian García <ccgarciab@unal.edu.co>
2020-06-08 03:31:06 -0500

    Added support for sessions in the registration route. Also refactored the error handling.

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import config from 'config';
 import cors from 'cors';
 import express from 'express';
+import MongoStore from 'connect-mongo';
 import mongoose from 'mongoose';
+import ExpressSession from 'express-session';
 
 import authRouter from './routes/auth';
 
 const port = Number.parseInt(config.get('port'));
 const dbURI : string =  config.get('dbURI');
+const secret : string = config.get('sessionSecret');
 
 const app = express();
 
@@ -24,6 +27,21 @@ let mongoOpts = {
 mongoose.connect(dbURI, mongoOpts)
     .then(() => console.log("Connected to DB"))
     .catch((err) => console.error(err));
+
+const mongoStore = MongoStore(ExpressSession)
+
+app.use(ExpressSession({
+    secret,
+    store : new mongoStore({
+        mongooseConnection : mongoose.connection
+    }),
+    cookie : {
+        httpOnly : true,
+        maxAge   : 1000 * 3600 * 24
+    },
+    saveUninitialized : false,
+    resave : false
+}))
 
 app.use('/', authRouter);
 
--- routes/auth.ts
+++ routes/auth.ts
@@ -2,41 +2,45 @@ import bcrypt from 'bcrypt';
 import Router from 'express';
 
 import User from '../models/user.model';
+import session from 'express-session';
 
 const authRouter = Router();
+
+function isString(obj : unknown) : obj is String{
+
+    return typeof obj === 'string';
+}
 
 authRouter.post('/register', async (req, res) => {
 
     const { username, email, password } = req.body;
 
-    if(!(username && email && password)) return res.status(400);
+    if(![username, email, password].every((e) => isString(e)))
+        return res.status(400).json("Improper data format");
+
+    const user = await User.findOne({ email });
+    if(user) return res.status(409).json({message : "User aready exists"});
+
+    const salt = await bcrypt.genSalt(10);
+    if(!salt) return res.status(500).json({message : "Internal server error"});
+
+    const hash = await bcrypt.hash(password, salt);
+    if(!hash) return res.status(500).json({message : "Internal server error"});
 
     let newUser = new User({
 
         email,
         username,
+        password : hash
     });
-
-    try{
-
-        const salt = await bcrypt.genSalt(10);
-        if(!salt) return res.status(500).json({message : "Internal server error"});
-    
-        const hash = await bcrypt.hash(password, salt);
-        if(!hash) return res.status(500).json({message : "Internal server error"});
-
-        newUser.password = hash;
-    }
-    catch{
-
-        return res.status(500).json({message : "Possibly malformed input data"});
-    }
 
     let savedUser = await newUser.save();
     if(!savedUser) return res.status(503).json({message : "Service unavailable"});
 
-    const user = await User.findOne({ email });
-    if(user) return res.status(409).json({message : "User aready exists"});
+    if(req.session){
+
+        req.session.userID = savedUser._id;
+    }
 
     return res.status(200).json({
         username : savedUser.username,
