3656932236a8a368c307421e16778460d9f06067
Cristian García <ccgarciab@unal.edu.co>
2020-06-08 04:08:18 -0500

    Added route for log in, with session support.

--- routes/auth.ts
+++ routes/auth.ts
@@ -15,17 +15,19 @@ authRouter.post('/register', async (req, res) => {
 
     const { username, email, password } = req.body;
 
-    if(![username, email, password].every((e) => isString(e)))
+    if(!(isString(username) && isString(password) && isString(email))){
+
         return res.status(400).json("Improper data format");
+    }
 
     const user = await User.findOne({ email });
-    if(user) return res.status(409).json({message : "User aready exists"});
+    if(user) return res.status(409).json("User aready exists");
 
     const salt = await bcrypt.genSalt(10);
-    if(!salt) return res.status(500).json({message : "Internal server error"});
+    if(!salt) return res.status(500).json("Internal server error");
 
     const hash = await bcrypt.hash(password, salt);
-    if(!hash) return res.status(500).json({message : "Internal server error"});
+    if(!hash) return res.status(500).json("Internal server error");
 
     let newUser = new User({
 
@@ -35,17 +37,37 @@ authRouter.post('/register', async (req, res) => {
     });
 
     let savedUser = await newUser.save();
-    if(!savedUser) return res.status(503).json({message : "Service unavailable"});
+    if(!savedUser) return res.status(503).json("Service unavailable");
 
-    if(req.session){
-
-        req.session.userID = savedUser._id;
-    }
+    if(req.session) req.session.userID = savedUser._id;
 
     return res.status(200).json({
         username : savedUser.username,
         email    : savedUser.email
     });
 });
+
+authRouter.post('/login', async (req, res) => {
+
+    const { username, email, password } = req.body;
+
+    if(!(isString(username) && isString(password) && isString(email))){
+
+        return res.status(400).json("Improper data format");
+    }
+
+    const user = await User.findOne({ email });
+    if(!user) return res.status(401).json("Incorrect log in");
+
+    const passMatch = await bcrypt.compare(password, user.password.valueOf());
+    if(!passMatch) return res.status(401).json("Incorrect log in");
+
+    if(req.session) req.session.userID = user._id;
+
+    return res.status(200).json({
+        username : user.username,
+        email    : user.email
+    });
+})
 
 export default authRouter;
